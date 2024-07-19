01826a07caa617f47b9c267dea6cecc3d39dd449
jjm <48465915+jaeminjeon123@users.noreply.github.com>
2024-07-19 22:51:05 +0900

    [Silver I] Title: 트리 순회, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,30 @@
+def add_node(tree, item, left, right):
+    tree[item] = (left, right)
+
+def preorder(tree, node):
+    if node == '.':
+        return ''
+    left, right = tree[node]
+    return node + preorder(tree, left) + preorder(tree, right)
+
+def inorder(tree, node):
+    if node == '.':
+        return ''
+    left, right = tree[node]
+    return inorder(tree, left) + node + inorder(tree, right)
+
+def postorder(tree, node):
+    if node == '.':
+        return ''
+    left, right = tree[node]
+    return postorder(tree, left) + postorder(tree, right) + node
+
+n = int(input())
+tree = {}
+for _ in range(n):
+    item, left, right = input().split()
+    add_node(tree, item, left, right)
+
+print(preorder(tree, 'A'))
+print(inorder(tree, 'A'))
+print(postorder(tree, 'A'))
